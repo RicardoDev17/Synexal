da3f49cd8e3864dee879df13b4b91fbd932fdaf2
RicardoDev17 <ricardoperezdev@gmail.com>
2025-09-05 09:33:28 -0600

    Estilos iniciales de la navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,20 @@
+import logo from "../assets/SynexalPNG (2).png";
+import "../styles/Navbar.css";
+
 export default function Navbar() {
   return (
-    <nav>
-      Navbar
+    <nav class="navbar">
+      <div class="logo">
+        <a href="#inicio">
+          <img src={logo} alt="Synexal Logo" style={{ height: "50px" }} />
+        </a>
+      </div>
+      <ul class="nav-links">
+        <li><a href="#inicio">Inicio</a></li>
+        <li><a href="#sobre-nosotros">Sobre nosotros</a></li>
+        <li><a href="#servicios">Servicios</a></li>
+        <li><a href="#contacto">Contacto</a></li>
+      </ul>
     </nav>
   );
 }
